8e187228aa98f5d120522f3c6c541654404ec512
Md. Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2024-10-12 00:51:21 +0600

    Made a demo to test

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(){
+	return(
+		<footer>
+		<p>SkillConnect. All rights reserved.</p>
+		</footer>
+	);
+}
+
+export default Footer;
+
---
+++ frontend/src/components/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MainContent(){
+	return(
+		<main>
+			<h2>This is the Main Content Are.</h2>
+			<p>Here there is more to add.</p>
+		</main>
+	);
+}
+
+export default MainContent;
+
+
